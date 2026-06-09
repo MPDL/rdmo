5ffc65b78dde7fd7b64a9babfe410645c8735d73
Laura Bahamon <bahamon@mpdl.mpg.de>
2026-06-09 14:04:22 +0200

    revert change to IntegrationForm (issue created https://github.com/rdmorganiser/rdmo/issues/1608)

--- rdmo/projects/forms.py
+++ rdmo/projects/forms.py
@@ -371,12 +371,8 @@ class IntegrationForm(forms.ModelForm):
 
             if field.get('placeholder'):
                 attrs = {'placeholder': field.get('placeholder')}
-            
-            widget = forms.TextInput(attrs=attrs)
-            if field.get('widget'):
-                widget = field.get('widget')
 
-            self.fields[field.get('key')] = forms.CharField(widget=widget,
+            self.fields[field.get('key')] = forms.CharField(widget=forms.TextInput(attrs=attrs),
                                                             initial=initial, required=field.get('required', True),
                                                             help_text=field.get('help'))
 
